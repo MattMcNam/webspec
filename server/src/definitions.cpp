29a6dcaeed2dccfc9ae574b86d84901cb06b22e4
Matthew McNamara <matt@mattmcn.com>
2013-07-30 08:50:58 +0100

    Reverting previous commits, they will be placed in a new branch.

--- server/src/definitions.cpp
+++ server/src/definitions.cpp
@@ -9,12 +9,11 @@
  */
 
 #include "definitions.h"
-#include <string.h>
 
 int GetEventIntForName(const char *name) {
 	int i = -1;
 	while (eventInts[++i]) {
-		if (!strcmp(name, eventInts[i])) return i;
+		if (!stricmp(name, eventInts[i])) return i;
 	}
 	return -1;
 }
